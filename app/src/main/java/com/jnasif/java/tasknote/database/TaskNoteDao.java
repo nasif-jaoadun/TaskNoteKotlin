77bc559630c3a8d90d1628b2709c7f0478e7ca38
nasif-jaoadun <nasif_online@yahoo.com>
2024-01-08 23:29:36 +0600

    Data Access Object

--- app/src/main/java/com/jnasif/java/tasknote/database/TaskNoteDao.java
+++ app/src/main/java/com/jnasif/java/tasknote/database/TaskNoteDao.java
@@ -1,4 +1,37 @@
 package com.jnasif.java.tasknote.database;
 
-public class TaskNoteDao {
+
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+@Dao
+public interface TaskNoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertTaskNote(TaskNoteEntity taskNoteEntity);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertAllTaskNote(List<TaskNoteEntity> taskNotes);
+
+    @Delete
+    void deleteTaskNote(TaskNoteEntity taskNoteEntity);
+
+    @Query("SELECT * FROM taskNotes WHERE id = :id")
+    TaskNoteEntity getTaskNoteById(int id);
+
+    @Query("SELECT * FROM taskNotes ORDER BY createDate DESC")
+    LiveData<List<TaskNoteEntity>> getAll();
+
+    @Query("DELETE FROM taskNotes")
+    int deleteALl();
+
+    @Query("SELECT COUNT(*) FROM taskNotes")
+    int getCount();
+
 }

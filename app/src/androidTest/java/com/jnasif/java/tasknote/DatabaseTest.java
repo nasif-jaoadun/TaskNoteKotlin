c2548b67e06c43ae4eebc2eef23c28234b8c03fd
nasif-jaoadun <nasif_online@yahoo.com>
2024-01-22 00:48:29 +0600

    Database Test class, create DB, Close DB, InsertAndRetrieve sample Data from DB, test with size of the database. debug ready for first test

---
+++ app/src/androidTest/java/com/jnasif/java/tasknote/DatabaseTest.java
@@ -0,0 +1,46 @@
+package com.jnasif.java.tasknote;
+
+import android.content.Context;
+import android.nfc.Tag;
+import android.util.Log;
+
+import com.jnasif.java.tasknote.database.AppDatabase;
+import com.jnasif.java.tasknote.database.TaskNoteDao;
+import com.jnasif.java.tasknote.utilities.SampleData;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.room.Room;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class DatabaseTest {
+    public static final String TAG = "Junit";
+    private AppDatabase mDb;
+    private TaskNoteDao mDao;
+    @Before
+    public void createDb(){
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext(); //Context reference without using Application context
+        mDb = Room.inMemoryDatabaseBuilder(context, AppDatabase.class).build(); //instance of Database in memory, which is used to not save persistent data and destroy after each use off this method
+        mDao = mDb.taskNoteDao();
+        Log.i(TAG, "createDb");
+    }
+    @After
+    public void closeDb(){
+        mDb.close();
+        Log.i(TAG, "closeDb");
+    }
+    @Test
+    public void createAndRetrieveTaskNotes(){
+        mDao.insertAllTaskNote(SampleData.getTaskNotes());
+        int count = mDao.getCount();
+        Log.i(TAG, "createAndRetrieveTaskNotes: count = " + count);
+        assertEquals(SampleData.getTaskNotes().size(), count);
+    }
+}
